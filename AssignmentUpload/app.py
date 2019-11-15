5b014c15b2e7444aa920dd61c02d83e73bb72b15
ShirishaSRao <31768648+ShirishaSRao@users.noreply.github.com>
2019-11-15 10:53:26 +0530

    Add files via upload

---
+++ AssignmentUpload/app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, Markup, request, redirect,jsonify,abort
+import requests
+import os
+from werkzeug import secure_filename
+from flask_cors import CORS
+from flask_static_compress import FlaskStaticCompress
+import logging
+from flask_pymongo import PyMongo
+import pymongo
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer
+import glob
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["UPLOAD_FOLDER"]="/Users/shirishasrao/Desktop/AssignmentUpload/database"
+app.config["MONGO_URI"]= "mongodb+srv://dbShirisha:myatlasdatabase@cluster0-pb58c.mongodb.net/test?retryWrites=true&w=majority"
+mongo = pymongo.MongoClient('mongodb+srv://dbShirisha:myatlasdatabase@cluster0-pb58c.mongodb.net/test?retryWrites=true&w=majority', maxPoolSize=50, connect=False)
+
+def plagiarism_check():
+	file_list=glob.glob("/Users/shirishasrao/Desktop/AssignmentUpload/database/*.txt")
+	corpus=[]
+	for file_path in file_list:
+		with open(file_path) as f_input:
+			corpus.append(f_input.read())
+	tfidf = TfidfVectorizer().fit_transform(corpus)
+	pairwise_similarity = tfidf * tfidf.T
+	a= pairwise_similarity.toarray()   
+	print(a)
+	n=len(corpus)
+	for i in range(n-1):
+		if(a[n-1][i]>0.75):
+			return a[n-1][i]
+	return 0		
+
+db = pymongo.database.Database(mongo, 'user_credentials')
+usercol = pymongo.collection.Collection(db, 'user_credentials')
+
+@app.route('/login',methods=['POST'])
+def login():
+    username = request.form['username'];
+    password = request.form['password'];
+    x=usercol.find_one({"username":username})
+    if(not(x)):
+        return "Username does not exists!",400
+    if(x):
+        if(x['password']==password):
+            return "",201
+        else:
+            return "Password Incorrect!",400
+
+
+@app.route('/upload')
+def upload_file_home():
+   return render_template('test.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method =='POST':
+        f = request.files['file']
+        if f:
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+            t=plagiarism_check()
+            if(t==0):
+              return 'File uploaded successfully'
+            else:
+              path=os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))
+              os.remove(path)
+              return 'Plagiarism detected. Please re-upload.'
+        else:
+        	return 'error while uploading. Please re-upload'    
+
+if __name__ == '__main__':
+   app.run(debug = True)
